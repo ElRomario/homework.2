bd2f77b65636b407e3a0b94f9d514c4f88d292e4
ElRomario <hellishassailant007@gmail.com>
2023-02-27 17:31:41 +0300

    homework done

---
+++ task2/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+
+
+int[] fromIntToArray(int num)
+{
+    int[] array = new int[num.ToString().Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = num % 10;
+        num /= 10;
+    }
+    Array.Reverse(array);
+    return array;
+}
+
+
+void print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        else Console.Write(", ");
+        Console.Write(array[i]);
+        if (i == array.Length - 1) Console.Write("]");
+    }
+}
+
+bool thirdNumExist(int[] array)
+{
+   if(array.Length >= 3)return true;
+   else return false;
+}
+int thirdNumbGetter(int[] array)
+{
+    return array[2];
+}
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+int[] array = fromIntToArray(num);
+
+if(thirdNumExist(array)) 
+{
+
+    Console.Write("Третье число есть! ");
+    Console.WriteLine($"->{thirdNumbGetter(array)}");
+
+}
+else Console.WriteLine("Третьего числа нет!");
+
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+bool dayfOfTheWeek(int day)
+{
+    
+    switch(day)
+    {
+        case 1: return false;break;
+        case 2: return false;break;
+        case 3: return false;break;
+        case 4: return false;break;
+        case 5: return false;break;
+        case 6: return true;break;
+        case 7: return true;break;
+        default: return false;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите номер дня недели:");
+int day = int.Parse(Console.ReadLine());
+
+if(dayfOfTheWeek(day)) Console.WriteLine($"{day}й день является выходным");
+else Console.WriteLine($"{day}й день не является выходным");
+
+
+
